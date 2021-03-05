c783d046b60589e4d71ef0d7928c7b41e2cb3d62
Fadil <fadilsiddique@gmail.com>
2021-03-05 05:09:40 +0000

    pdf invoice url

--- erpnext_pos_sms/sms.py
+++ erpnext_pos_sms/sms.py
@@ -4,13 +4,16 @@ from frappe.utils import cint, cstr, getdate, today
 from frappe import throw, _
 from frappe.utils.nestedset import NestedSet, get_ancestors_of, get_descendants_of
 import json 
+import time
 
 
 
 def send_sms(doc, event):
     mobile_number = frappe.db.get_value('Customer', {'name': doc.customer}, ['mobile_number'])
     from frappe.core.doctype.sms_settings.sms_settings import send_sms
-    message = "Thank you for your purchase at Frapino. You have spent Rs " + str(doc.grand_total) + " via Cash on : "  + (doc.posting_date)
+    url = "http://frapino.tridz.in/files/{}.pdf".format(doc.name)
+    
+    message = "Thank you for your purchase at Frapino. You have spent Rs " + str(doc.grand_total) + " via Cash on : "  + (doc.posting_date) + "."+" " + "Click the link to view your invoice" + " " + url
     send_sms([mobile_number], msg=message)
 
     
