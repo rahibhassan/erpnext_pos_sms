8089ac75c1528b4741bdabc0ccc89a75cfe05881
Fadil <fadilsiidique@gmail.com>
2021-04-23 16:18:41 +0000

    revert to old template

--- erpnext_pos_sms/sms.py
+++ erpnext_pos_sms/sms.py
@@ -5,15 +5,23 @@ from frappe import throw, _
 from frappe.utils.nestedset import NestedSet, get_ancestors_of, get_descendants_of
 import json 
 import time
+import urllib
+from urllib.request import urlretrieve
 
 
 
 def send_sms(doc, event):
-    mobile_number = frappe.db.get_value('Customer', {'name': doc.customer}, ['mobile_number'])
+    mobile_number = frappe.db.get_value('Customer', {'name': doc.customer}, ['mobile_no'])
     from frappe.core.doctype.sms_settings.sms_settings import send_sms
-    url = "http://store1.frapino.in/files/{}.pdf".format(doc.name)
+    #url = "http://store1.frapino.in/files/{}.pdf".format(doc.name)
+    sales =doc.doctype
+    sales = sales.replace(" ","+")
+
+    #url = "http://" + frappe.request.host + "/api/method/frappe.utils.print_format.download_pdf?doctype="+sales +"&name={}".format(doc.name)+"&format=Natural"+"&no_letterhead=0"
+    #url = "http://store1.frapino.in/files/{}.pdf".format(doc.name)
     
-    message = "Thank you for your purchase at Frapino. You have spent Rs " + str(doc.grand_total) + " via Cash on : "  + (doc.posting_date) + "."+" " + "Click the link to view your invoice" + " " + url
+    #message = "Thank you for your purchase at Frapino. You have spent Rs " + str(doc.grand_total) + " via Cash on : "  + (doc.posting_date) + "."+" " + "Click the link to view your invoice" + " "+ url
+    message = "Thank you for your purchase at Frapino. You have spent Rs " + str(doc.grand_total) + " via Cash on : "  + (doc.posting_date)
     send_sms([mobile_number], msg=message)
 
     
