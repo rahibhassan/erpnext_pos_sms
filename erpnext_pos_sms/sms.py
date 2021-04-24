9e4c9ca3a150504c03559265d8a0633551bb34e8
Fadil <fadilsiidique@gmail.com>
2021-04-24 00:47:37 +0000

    template commit

--- erpnext_pos_sms/sms.py
+++ erpnext_pos_sms/sms.py
@@ -13,15 +13,11 @@ from urllib.request import urlretrieve
 def send_sms(doc, event):
     mobile_number = frappe.db.get_value('Customer', {'name': doc.customer}, ['mobile_no'])
     from frappe.core.doctype.sms_settings.sms_settings import send_sms
-    #url = "http://store1.frapino.in/files/{}.pdf".format(doc.name)
-    sales =doc.doctype
-    sales = sales.replace(" ","+")
-
-    #url = "http://" + frappe.request.host + "/api/method/frappe.utils.print_format.download_pdf?doctype="+sales +"&name={}".format(doc.name)+"&format=Natural"+"&no_letterhead=0"
+    
     #url = "http://store1.frapino.in/files/{}.pdf".format(doc.name)
     
     #message = "Thank you for your purchase at Frapino. You have spent Rs " + str(doc.grand_total) + " via Cash on : "  + (doc.posting_date) + "."+" " + "Click the link to view your invoice" + " "+ url
-    message = "Thank you for your purchase at Frapino. You have spent Rs " + str(doc.grand_total) + " via Cash on : "  + (doc.posting_date).
+    message = "Thank you for your purchase at Frapino. You have spent Rs " + str(doc.grand_total) + " via Cash on :" + (doc.posting_date)
     send_sms([mobile_number], msg=message)
 
     
