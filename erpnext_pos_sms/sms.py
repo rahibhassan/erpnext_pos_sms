b858cfab2b100de67c195b2cb1efc14b2ab59970
Fadil <fadilsiidique@gmail.com>
2021-04-28 15:50:18 +0000

    sms issue fixed

--- erpnext_pos_sms/sms.py
+++ erpnext_pos_sms/sms.py
@@ -3,21 +3,19 @@ import frappe
 from frappe.utils import cint, cstr, getdate, today
 from frappe import throw, _
 from frappe.utils.nestedset import NestedSet, get_ancestors_of, get_descendants_of
-import json 
-import time
-import urllib
-from urllib.request import urlretrieve
+import json
+from erpnext_pos_sms import url_shorten
 
 
 
 def send_sms(doc, event):
-    mobile_number = frappe.db.get_value('Customer', {'name': doc.customer}, ['mobile_no'])
+    mobile_number = frappe.db.get_value('Customer', {'name': doc.customer}, ['mobile_number'])
     from frappe.core.doctype.sms_settings.sms_settings import send_sms
-    
+    #url = 
     url = "http://store1.frapino.in/files/{}.pdf".format(doc.name)
-    
-    message = "Thank you for your purchase at Frapino. You have spent Rs " + str(doc.grand_total) + " via Cash on : "  + (doc.posting_date) + "."+" " + "Click the link to view your invoice" + " "+ url
-    #message = "Thank you for your purchase at Frapino. You have spent Rs " + str(doc.grand_total) + " via Cash on :" + (doc.posting_date)
+    url_short = url_shorten.make_shorten(url)
+    message = "Thank you for your purchase at Frapino. You have spent Rs " + str(doc.grand_total) +"via cash on "+ (doc.posting_date)+" "+"."+" Click the link to view your invoice "+ url_short+"."
     send_sms([mobile_number], msg=message)
+    
 
     
